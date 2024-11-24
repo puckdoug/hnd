f45bf024a702382574e39263dd81bfb6683ff85e
Doug Morris <doug@mhost.com>
2024-11-24 22:30:32 +0100

    moved command-line parsing into a module

---
+++ src/cli/mod.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+pub struct Args {
+    /// Override the default configuration file
+    #[arg(short, long, default_value = crate::config::CONFIG)]
+    pub config: String,
+
+    /// Check the configuration file
+    #[arg(long)]
+    pub check: bool,
+
+    /// Specify an alternate log file
+    #[arg(long, short, default_value = crate::config::LOGFILE)]
+    pub logfile: String,
+
+    /// Set the log level
+    #[arg(long, default_value = crate::config::LOGLEVEL)]
+    pub loglevel: String,
+
+    #[arg(short, long)]
+    pub verbose: bool,
+}
+
+pub fn parse() -> Args {
+    let args = Args::parse();
+
+    args
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate log;
 
-use clap::Parser;
-// use log::Level;
 use log::{info, LevelFilter}; // , SetLoggerError};
 use syslog::{BasicLogger, Facility, Formatter3164};
 
+mod cli;
 mod config;
 mod dhcp_client;
 mod dhcp_server;
@@ -12,31 +11,8 @@ mod dns_client;
 mod dns_server;
 mod hnd_p2p;
 
-#[derive(Parser, Debug)]
-#[command(version, about)]
-struct Args {
-    /// Override the default configuration file
-    #[arg(short, long, default_value = crate::config::CONFIG)]
-    config: String,
-
-    /// Check the configuration file
-    #[arg(long)]
-    check: bool,
-
-    /// Specify an alternate log file
-    #[arg(long, short, default_value = crate::config::LOGFILE)]
-    logfile: String,
-
-    /// Set the log level
-    #[arg(long, default_value = crate::config::LOGLEVEL)]
-    loglevel: String,
-
-    #[arg(short, long)]
-    verbose: bool,
-}
-
 fn main() {
-    let args = Args::parse();
+    let args = cli::parse();
 
     let log_level = match args.loglevel.as_str() {
         "error" => LevelFilter::Error,
